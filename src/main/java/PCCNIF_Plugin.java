286e42502831d47eeeb6c245c2b54526dc66a383
Joaquin Bengochea <joackobengochea@gmail.com>
2015-07-11 20:30:09 -0300

    asdf

--- src/main/java/PCCNIF_Plugin.java
+++ src/main/java/PCCNIF_Plugin.java
@@ -27,6 +27,7 @@
 
 import ij.*;
 import ij.io.OpenDialog;
+import ij.measure.ResultsTable;
 import ij.plugin.ImageCalculator;
 import ij.plugin.PlugIn;
 import ij.process.ImageProcessor;
@@ -36,6 +37,7 @@ import loci.plugins.BF;
 import loci.plugins.in.ImporterOptions;
 import java.io.File;
 import java.io.IOException;
+import java.util.Vector;
 
 /**
  * A very simple example of using Bio-Formats in an ImageJ plugin.
@@ -43,10 +45,15 @@ import java.io.IOException;
 public class PCCNIF_Plugin implements PlugIn {
 
     // Paths and file names
-    private String dir = "C:\\Data\\PDI\\";
+    private String dir = "/Users/joaquinbengochea/Facultad/PDI/cziImages/";
     private String resultsPath = dir + "results";
-    private String name = "Exp035_D5_a_dcxcy3_flagcy2_bIIItubcy5_dapi_20X.czi";
+    private String name = "Exp035_C5_a_dcxcy3_flagcy2_bIIItubcy5_dapi_20X.czi";
     String id = dir + name;
+
+    ResultsTable nucleos;
+    ResultsTable verdes;
+
+
 
     public void run(String arg) {
         // Open czi image with Bio-Formats
@@ -127,7 +134,7 @@ public class PCCNIF_Plugin implements PlugIn {
         IJ.run(redChannel, "Subtract Background...", "rolling=50");
 
         // Threshold with black background
-        IJ.setAutoThreshold(redChannel, "Default");
+        IJ.setAutoThreshold(redChannel, "Default dark");
         Prefs.blackBackground = true;
 
         // Convert to mask
@@ -139,6 +146,8 @@ public class PCCNIF_Plugin implements PlugIn {
         // Apply watershed filter to separate nuclei clusters
         IJ.run(redChannel, "Watershed", "");
 
+        IJ.run(redChannel, "Analyze Particles...", "size=10-Infinity circularity=0.40-1.00 show=Outlines display clear record in_situ");
+        nucleos = (ResultsTable) ResultsTable.getResultsTable().clone();
         return redChannel;
     }
 
@@ -162,8 +171,37 @@ public class PCCNIF_Plugin implements PlugIn {
         Prefs.blackBackground = true;
         IJ.run(imp, "Convert to Mask", "");
         IJ.run(imp, "Analyze Particles...", "size=10-Infinity circularity=0.40-1.00 show=Outlines display clear record in_situ");
-
+        verdes = (ResultsTable) ResultsTable.getResultsTable().clone();
         // Return imp modified
+        double[] nucleosX = nucleos.getColumnAsDoubles(ResultsTable.X_CENTER_OF_MASS);
+        double[] nucleosY = nucleos.getColumnAsDoubles(ResultsTable.Y_CENTER_OF_MASS);
+        double[] areas = nucleos.getColumnAsDoubles(ResultsTable.AREA);
+        double[] CIs = nucleos.getColumnAsDoubles(ResultsTable.ROUNDNESS);
+        double[] verdesX = verdes.getColumnAsDoubles(ResultsTable.X_CENTER_OF_MASS);
+        double[] verdesY = verdes.getColumnAsDoubles(ResultsTable.Y_CENTER_OF_MASS);
+        System.out.println(verdesX);
+        Vector<Vector<Double>> result = new Vector<Vector<Double>>();
+        for (int i=0; i<verdesX.length; i++){
+            System.out.println(i);
+            boolean found = false;
+            for(int j=0; j<nucleosX.length; j++){
+                if(Math.abs(nucleosX[j]-verdesX[i])<1500 && Math.abs(nucleosY[j]-verdesY[i])<1500){
+                    double radio = areas[j]/Math.PI;
+                    double dist = Math.pow(nucleosX[j]-verdesX[i], 2) + Math.pow(nucleosY[j]-verdesY[i], 2);
+
+                    if(dist<= radio+ 2*radio*CIs[j]+CIs[j]*CIs[j]){
+                        Vector<Double> v = new Vector<Double>();
+                        v.add(verdesX[i]);
+                        v.add(verdesY[i]);
+                        result.add(v);
+                        found = true;
+                    }
+
+                }
+                if(found) break;
+            }
+        }
+        System.out.println(result);
         return imp;
 
     }
